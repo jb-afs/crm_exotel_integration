12896b9000b51b1b700219d841daf71313440ade
tailorraj <tailorraj111@gmail.com>
2024-03-12 14:27:52 +0500

    new featured added in lead doctype to initiate call functionality.

--- crm_exotel_integration/api/call.py
+++ crm_exotel_integration/api/call.py
@@ -2,10 +2,14 @@ import frappe
 import requests
 import json
 
+# Define callback function
+# make call details get call from callback Url
+# update call data
+
 
 
 @frappe.whitelist()
-def initiating_call_numbers(from_no, to_no, caller_id, call_type):
+def initiating_call_numbers(from_no, to_no, caller_id):
     try:
         exotel_details = frappe.get_doc("CRM Exotel Settings")
         if not exotel_details.enabled:
@@ -17,31 +21,25 @@ def initiating_call_numbers(from_no, to_no, caller_id, call_type):
         subdomain = exotel_details.subdomain
         app_id = exotel_details.app_id
 
-        logs_url = f"https://{exotel_api_key}:{exotel_api_token}@{subdomain}/v1/Accounts/{exotel_account_sid}/Calls/connect"
-        # logs_url = f"https://{subdomain}/v1/Accounts/{exotel_account_sid}/Calls/connect"
-
-        # frappe.log_error(title="Exotel API Request", message=f"From: {from_no}, To: {to_no}, CallerId: {caller_id}, CallType: {call_type}")
+        call_url = f"https://{exotel_api_key}:{exotel_api_token}@{subdomain}/v1/Accounts/{exotel_account_sid}/Calls/connect"
+       
         
         data = {
             'From': from_no,
             'To': to_no,
-            'CallerId': caller_id,
-            'CallType': call_type,
-            'Url': f"http://my.exotel.com/{exotel_account_sid}/exoml/start_voice/{app_id}"
+            'CallerId': caller_id
         }
         auth = (exotel_api_key, exotel_api_token)
-        res = requests.post(logs_url, data=data, auth=auth)
-
-        frappe.log_error("post call initiated",message=res.content)
+        res = requests.post(call_url, data=data, auth=auth)
 
         if res.status_code == 200:
             if res.content:
                 res_json = res.json()
-                frappe.log_error(title="Exotel API Response", message=res_json)
+                # frappe.log_error(title="Exotel API Response", message=res_json)
             else:
                 frappe.throw("Empty response received from Exotel API")
         else:
-            frappe.log_error(title="API Debug Information", message=f"URL: {logs_url}, Status Code: {res.status_code}, Response Content: {res.text}")
+            frappe.log_error(title="API Debug Information", message=f"URL: {call_url}, Status Code: {res.status_code}, Response Content: {res.text}")
             frappe.throw(f"API call failed with status code {res.status_code}")
     except Exception as e:
         frappe.log_error(title="Exotel Api Call Failed", message=str(e))
